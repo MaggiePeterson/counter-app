ed42694077682cc8909fb0d0161d3fafe982665d
MaggiePeterson <margaret.peterson@gmail.com>
2021-01-25 21:10:31 -0800

    update count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,20 +4,19 @@ class Counter extends Component {
 
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
     }; 
-    
-    renderTags(){
-        if( this.state.tags.length === 0) return <p> There are no tags!</p>;
 
-        return <ul> {this.state.tags.map(tag => <li key ={tag}> { tag}</li>)} </ul>;
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1})
     }
+    
    
     render() { 
 
         return (
             <div>
-                {this.renderTags()};
+                <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment</button>
             </div>
         );
     }
